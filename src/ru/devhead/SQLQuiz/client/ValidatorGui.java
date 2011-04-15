ed493ff50a90598f8dd270b62d7db11bb93b06f4
Andrey Kuznecov <andreykyz@gmail.com>
2011-04-15 19:08:39 +0400

    Начал реализовывать запрос json объекта с сервера

--- src/ru/devhead/SQLQuiz/client/ValidatorGui.java
+++ src/ru/devhead/SQLQuiz/client/ValidatorGui.java
@@ -4,104 +4,146 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.ui.*; 
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.http.client.URL;
+import com.google.gwt.user.client.ui.*;
 
 public class ValidatorGui implements EntryPoint {
-	
-	//Компоненты дизайна
+
+	private static final String JSON_BASE_URL = "http://vz.devhead.ru:7080/";
+	private static final String JSON_URL = JSON_BASE_URL + "getqueryresult?q=";
+
+	// Компоненты дизайна
 	HorizontalPanel upperPanel = new HorizontalPanel();
 	VerticalPanel buttonPanel = new VerticalPanel();
 	VerticalPanel mainPanel = new VerticalPanel();
 	HorizontalPanel tablePanel = new HorizontalPanel();
-	
+
 	Button executeButton = new Button("Execute query");
 	Button getResultButton = new Button("Get result");
-	
+
 	TextArea queryPlace = new TextArea();
-	
+
 	Label queryResult = new Label();
 	Label correctQueryResult = new Label();
-	
-	FlexTable tableQueryResult = new FlexTable(); 
+
+	FlexTable tableQueryResult = new FlexTable();
 	FlexTable tableSolution = new FlexTable();
-	
-	String[][] tableModelQueryResult; 
-	
+
+	String[][] tableModelQueryResult;
+
 	@Override
 	public void onModuleLoad() {
-		
+
 		getResultButton.addStyleName("getResultButton");
-		
+
 		buttonPanel.add(executeButton);
 		buttonPanel.add(getResultButton);
 		buttonPanel.setStyleName("buttonPanel");
-		
+
 		queryPlace.setCharacterWidth(100);
 		queryPlace.setVisibleLines(5);
-				
+
 		upperPanel.add(queryPlace);
 		upperPanel.add(buttonPanel);
 
 		tableQueryResult.setText(0, 0, "data00");
-		
+
 		tablePanel.add(tableQueryResult);
 		tablePanel.add(tableSolution);
-		
+
 		tableQueryResult.addStyleName("flexTable");
-		
+
 		tableSolution.setText(4, 4, "4");
 		mainPanel.add(upperPanel);
 		mainPanel.add(tablePanel);
-		
+
 		RootPanel.get("validator").add(mainPanel);
-		
+
 		queryPlace.setFocus(true);
 
-		
 		executeButton.addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				
+
 				executeButtonClick();
 			}
 
 		});
 	}
-	
+
 	void executeButtonClick() {
-		tableQueryResult.clear(true);
-		
+
+		String url = URL.encode(JSON_URL + queryPlace.getText());
+
+		// Send request to server and catch any errors.
+		RequestBuilder builder = new RequestBuilder(RequestBuilder.GET, url);
+
+		try {
+			Request request = builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					displayError("Couldn't retrieve JSON");
+				}
+
+				public void onResponseReceived(Request request,
+						Response response) {
+					if (200 == response.getStatusCode()) {
+//						updateTable(asArrayOfStockData(response.getText()));
+					} else {
+						displayError("Couldn't retrieve JSON ("
+								+ response.getStatusText() + ")");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			displayError("Couldn't retrieve JSON");
+		}
+
 	}
-	
+
 	void fireTableQueryResultUpdate() {
-		
+
 		tableQueryResult.clear(true);
-		for (int i=0; (i<tableModelQueryResult.length) & (tableModelQueryResult != null); i++) {
-			for (int j=0; (j<tableModelQueryResult[0].length) & (tableModelQueryResult[0] != null); j++) {
+		for (int i = 0; (i < tableModelQueryResult.length)
+				& (tableModelQueryResult != null); i++) {
+			for (int j = 0; (j < tableModelQueryResult[0].length)
+					& (tableModelQueryResult[0] != null); j++) {
 				tableQueryResult.setText(i, j, tableModelQueryResult[i][j]);
 				if (i == 0) {
-					//Стиль для шапки таблицы
-					tableQueryResult.getRowFormatter().addStyleName(0, "flexTableHeader");
-				}
-				else {
-					//Стиль для данных таблицы
-					tableQueryResult.getCellFormatter().addStyleName(i, j, "flexTable");
+					// Стиль для шапки таблицы
+					tableQueryResult.getRowFormatter().addStyleName(0,
+							"flexTableHeader");
+				} else {
+					// Стиль для данных таблицы
+					tableQueryResult.getCellFormatter().addStyleName(i, j,
+							"flexTable");
 				}
 			}
 		}
 	}
 	
-	void fireTableSolutionUpdate() {
-		//Заглушка
-		tableQueryResult.clear(true);
+	void displayError(String er) {
+		tableModelQueryResult = new String[0][0];
+		tableModelQueryResult[0][0] = er;
+		
+		fireTableQueryResultUpdate();
 	}
 	
+	void fireTableSolutionUpdate() {
+		// Заглушка
+		tableQueryResult.clear(true);
+	}
+
 	/**
-	   * Convert the string of JSON into JavaScript object.
-	   */
-	  private final native JsArray asArrayOfStockData(String json) /*-{
-	    return eval(json);
-	  }-*/;
+	 * Convert the string of JSON into JavaScript object.
+	 */
+	// private final native JsArray asArrayOfStockData(String json) /*-{
+	// return eval(json);
+	// }-*/;
 
 }
